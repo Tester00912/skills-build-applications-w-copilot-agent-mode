64fe682f34f6c06cf7c84f845f9286aec67ff6b7
Tester00912 <cs22.subhankar.ray@stcet.ac.in>
2026-06-26 10:53:03 +0000

    Added Models DB and API structures

--- octofit-tracker/backend/database.ts
+++ octofit-tracker/backend/database.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-export const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/octofit_db';
+export const mongoUri = 'mongodb://127.0.0.1:27017/octofit_db';
 
 export async function connectToDatabase() {
   if (mongoose.connection.readyState >= 1) {
